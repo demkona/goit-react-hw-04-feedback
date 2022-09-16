de29c59d1316a1d09b1022ae6388d6c0d84bc2c0
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2022-09-17 01:05:02 +0300

    add html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1
+      }
+    })
+  }
+
+  handleNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1
+      }
+    })
+  }
+
+  handleBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Please leave feedback</h1>
+          <button type="button" onClick={this.handleGood}>Good</button>
+          <button type="button" onClick={this.handleNeutral}>Neutral</button>
+          <button type="button" onClick={this.handleBad}> Bad</button></div>
+        <div>
+          <h2>Statistics</h2>
+          <span>Good:{this.state.good}</span>
+          <span>Neutral:{this.state.neutral}</span>
+          <span>Bad:{this.state.bad}</span></div>
+      </div>
+    )
+  }
+
+}
